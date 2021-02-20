c88cc8154f10f0a5742ed63d72246d2ee9ff601d
PatrickMSM <calin.chertes@gmail.com>
2021-02-20 22:20:10 +0200

    added files i forgot to track (index v5 css and index v2 js)

---
+++ res/index_v2.js
@@ -0,0 +1,13 @@
+function alertDiscord() {
+	alert("PatrickMSM#9997");
+}
+
+
+function dropdown(dropdownelem) {
+	let content = dropdownelem.querySelector("#dropdowncontent");
+	if (content.style.display == "none") {
+		content.style.display = "block";
+	} else {
+		content.style.display = "none";
+	}
+}
---
+++ res/index_v2.packed.js
@@ -0,0 +1,1 @@
+function alertDiscord(){alert("PatrickMSM#9997")}function dropdown(a){let b=a.querySelector("#dropdowncontent");b.style.display="none"==b.style.display?"block":"none"}
